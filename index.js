60f8d7a1ea4ccf9613973702c23f220fc3ee33ca
Daniel Weinman <namniewd@gmail.com>
2019-02-16 23:13:08 -0500

    added dependencies and basic airtable helper

---
+++ index.js
@@ -0,0 +1,18 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+const name = 'Daniel Weinman';  // YOUR NAME HERE
+
+(async function example() {
+  let driver = new Builder().forBrowser('chrome').build();
+    try {
+    await driver.get('https://airtable.com/shr8ZYuNjevMLRsxI');
+    let student = driver.wait(until.elementLocated(By.className('addRecordSelector')), 5000)
+    await student.click();
+    let input = driver.wait(until.elementLocated(By.className('py2')), 5000)
+    await input.sendKeys(name);
+    await driver.sleep(1000);
+    await input.sendKeys(Key.RETURN);
+  } finally {
+    // fill out the rest of the form you bum
+  }
+})();
